8279d67d86ad63342f3afa6c794e4e284ab959de
Cathal Henchy <20091405@mail.wit.ie>
2022-03-11 22:22:08 +0000

    Finally have a functioning 'addImage' method

--- app/controllers/pdfs.js
+++ app/controllers/pdfs.js
@@ -4,8 +4,8 @@ const Boom = require("@hapi/boom");
 const Submission = require("../models/submission");
 const Joi = require("@hapi/joi");
 const sanitizeHtml = require("sanitize-html");
-//import { jsPDF } from "jspdf";
-const { jsPDF } = require("jspdf"); // will automatically load the node version
+const imageDataURI = require("image-data-uri");
+const { jsPDF } = require("jspdf");
 
 const Pdfs = {
   jsPdf: {
@@ -19,11 +19,18 @@ const Pdfs = {
   createPdf: {
     auth: false,
     handler: async function (request, h) {
+      //const imgData = imageDataURI.encodeFromFile("blackmirror.png").then((res) => console.log(res));
+      const imgData = await imageDataURI.encodeFromFile("public/images/ITPL.png");
+      //console.log("Holy Grail: " + imgData);
+
       const doc = new jsPDF("landscape");
       const user = await User.findById(request.params._id).lean();
       const submission = await Submission.findByUserId(user).lean();
       console.log("Created the following pdf: " + submission.projectTitle);
-      console.log("Creaded pdf for: " + user.firstName);
+      console.log("Created pdf for: " + user.firstName);
+
+      doc.addImage(imgData, "JPG", 100, 0, 30, 40);
+      doc.addImage(imgData, "JPG", 0, 0, 30, 40);
       doc.text(submission.projectTitle, 10, 10);
       doc.save(user.firstName + user.lastName + ".pdf");
       return h.view("report", {
