08d2f4e557976db606e7a031e777352536b64ae4
Cathal Henchy <20091405@mail.wit.ie>
2022-03-13 20:19:43 +0000

    Image upload functionality is now connected to jspdf functionality (jspdf was hitherto hard-coded to a specific image)

--- app/controllers/pdfs.js
+++ app/controllers/pdfs.js
@@ -19,18 +19,17 @@ const Pdfs = {
   createPdf: {
     auth: false,
     handler: async function (request, h) {
-      //const imgData = imageDataURI.encodeFromFile("blackmirror.png").then((res) => console.log(res));
-      const imgData = await imageDataURI.encodeFromFile("public/images/ITPL.png");
-      //console.log("Holy Grail: " + imgData);
-
+      //const imgData = await imageDataURI.encodeFromFile("public/images/ITPL.png");
       const doc = new jsPDF("landscape");
       const user = await User.findById(request.params._id).lean();
       const submission = await Submission.findByUserId(user).lean();
+      const personalPhotoImgData = await imageDataURI.encodeFromURL(submission.projectImage);
+      const projectImageImgData = await imageDataURI.encodeFromURL(submission.personalPhoto);
       console.log("Created the following pdf: " + submission.projectTitle);
       console.log("Created pdf for: " + user.firstName);
 
-      doc.addImage(imgData, "JPG", 100, 0, 30, 40);
-      doc.addImage(imgData, "JPG", 0, 0, 30, 40);
+      doc.addImage(personalPhotoImgData, "JPG", 100, 0, 30, 40);
+      doc.addImage(projectImageImgData, "JPG", 0, 0, 30, 40);
       doc.text(submission.projectTitle, 10, 10);
       doc.save(user.firstName + user.lastName + ".pdf");
       return h.view("report", {
