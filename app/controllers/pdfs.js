80ed0fc52ea98b7779651b759ce14637ca311b68
Cathal Henchy <20091405@mail.wit.ie>
2022-03-13 21:12:55 +0000

    Introduced error catching for jspdf upload, if any or the fields are empty

--- app/controllers/pdfs.js
+++ app/controllers/pdfs.js
@@ -19,23 +19,31 @@ const Pdfs = {
   createPdf: {
     auth: false,
     handler: async function (request, h) {
-      //const imgData = await imageDataURI.encodeFromFile("public/images/ITPL.png");
-      const doc = new jsPDF("landscape");
-      const user = await User.findById(request.params._id).lean();
-      const submission = await Submission.findByUserId(user).lean();
-      const personalPhotoImgData = await imageDataURI.encodeFromURL(submission.projectImage);
-      const projectImageImgData = await imageDataURI.encodeFromURL(submission.personalPhoto);
-      console.log("Created the following pdf: " + submission.projectTitle);
-      console.log("Created pdf for: " + user.firstName);
+      try {
+        //const imgData = await imageDataURI.encodeFromFile("public/images/ITPL.png");
+        const doc = new jsPDF("landscape");
+        const user = await User.findById(request.params._id).lean();
+        const submission = await Submission.findByUserId(user).lean();
+        const personalPhotoImgData = await imageDataURI.encodeFromURL(submission.projectImage);
+        const projectImageImgData = await imageDataURI.encodeFromURL(submission.personalPhoto);
+        console.log("Created the following pdf: " + submission.projectTitle);
+        console.log("Created pdf for: " + user.firstName);
 
-      doc.addImage(personalPhotoImgData, "JPG", 100, 0, 30, 40);
-      doc.addImage(projectImageImgData, "JPG", 0, 0, 30, 40);
-      doc.text(submission.projectTitle, 10, 10);
-      doc.save(user.firstName + user.lastName + ".pdf");
-      return h.view("report", {
-        title: "User's Submission",
-        submission: submission,
-      });
+        doc.addImage(personalPhotoImgData, "JPG", 100, 0, 30, 40);
+        doc.addImage(projectImageImgData, "JPG", 0, 0, 30, 40);
+        doc.text(submission.projectTitle, 10, 10);
+        doc.save(user.firstName + user.lastName + ".pdf");
+        return h.view("report", {
+          title: "User's Submission",
+          submission: submission,
+        });
+      } catch (err) {
+        console.log("Error updating Submission");
+        return h.view("js-pdf", {
+          title: "Submission Error",
+          errors: [{ message: err.message }],
+        });
+      }
     },
   },
 };
