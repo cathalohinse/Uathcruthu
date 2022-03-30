a461fa164d3b5fb6e0763b9d68bcb83bf3e4e295
Cathal Henchy <20091405@mail.wit.ie>
2022-03-30 20:52:00 +0100

    Created administrator model

---
+++ app/models/administrator.js
@@ -0,0 +1,40 @@
+"use strict";
+const Mongoose = require("mongoose");
+const Boom = require("@hapi/boom");
+const Schema = Mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  role: {
+    type: String,
+    default: "student",
+    enum: ["student", "administrator"],
+  },
+  accessToken: String,
+});
+
+userSchema.statics.findByEmail = function (email) {
+  return this.findOne({ email: email });
+};
+
+/*userSchema.methods.comparePassword = async function (categoryPassword) {
+  const isMatch = await bcrypt.compare(categoryPassword, this.password);
+  if (!isMatch) {
+    throw Boom.unauthorized("Incorrect Password entered");
+  }
+  return this;
+};*/
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const isMatch = this.password === candidatePassword;
+  if (!isMatch) {
+    throw Boom.unauthorized("Password mismatch");
+  }
+  return this;
+};
+
+module.exports = Mongoose.model("User", userSchema);
