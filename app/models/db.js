83234a8ab882f661080fcb4b79372ed21371d483
Cathal Henchy <20091405@mail.wit.ie>
2022-04-04 17:56:54 +0100

    admin access level (login, authentication & model) now functional

--- app/models/db.js
+++ app/models/db.js
@@ -22,3 +22,16 @@ db.on("disconnected", function () {
 db.once("open", function () {
   console.log(`database connected to ${this.name} on ${this.host}`);
 });
+
+async function seed() {
+  var seeder = require("mais-mongoose-seeder")(Mongoose);
+  const data = require("./seed-data.json");
+  const Admin = require("./admin");
+  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: false });
+  console.log(dbData);
+}
+
+/*db.once("open", function () {
+  console.log(`database connected to ${this.name} on ${this.host}`);
+  seed();
+});*/
