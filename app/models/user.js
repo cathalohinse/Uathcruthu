1e2f058e3f59d8abab48aef7cc737c7f55519a78
Cathal Henchy <20091405@mail.wit.ie>
2022-04-04 22:52:27 +0100

    introduced bcrypt and now all passwords are hashed and stored in salt rounds of 10.

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@
 const Mongoose = require("mongoose");
 const Boom = require("@hapi/boom");
 const Schema = Mongoose.Schema;
-//const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   firstName: String,
@@ -15,20 +15,20 @@ userSchema.statics.findByEmail = function (email) {
   return this.findOne({ email: email });
 };
 
-/*userSchema.methods.comparePassword = async function (categoryPassword) {
+userSchema.methods.comparePassword = async function (categoryPassword) {
   const isMatch = await bcrypt.compare(categoryPassword, this.password);
   if (!isMatch) {
     throw Boom.unauthorized("Incorrect Password entered");
   }
   return this;
-};*/
+};
 
-userSchema.methods.comparePassword = function (candidatePassword) {
+/*userSchema.methods.comparePassword = function (candidatePassword) {
   const isMatch = this.password === candidatePassword;
   if (!isMatch) {
     throw Boom.unauthorized("Password mismatch");
   }
   return this;
-};
+};*/
 
 module.exports = Mongoose.model("User", userSchema);
